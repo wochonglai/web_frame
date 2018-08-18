a58454222d63a2768e2fdfbec752f0469f794d03
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-18 10:09:13 +0800

    Update factorial_tornado.py

--- Tornado/factorial_tornado.py
+++ Tornado/factorial_tornado.py
@@ -4,26 +4,30 @@ import tornado
 import tornado.web
 
 
+
+# 定义一个阶乘服务对象
 class FactorialService(object):
   def __init__(self):
-    self.cache ={}
+    self.cache ={}  # 用字典记录已经计算过的阶乘
 
   def calc(self,n):
-    if n in self.cache:
+    if n in self.cache: # 如果有直接返回
       return self.cache[n]
     s = 1
     for i in range(1,n):
       s *= i
-    self.cache[n] = s
+    self.cache[n] = s # 缓存起来
     return s
 
 
 class FactorialHandler(tornado.web.RequestHandler):
-
+  #new出阶乘服务对象
   service = FactorialService()
 
   def get(self):
+    # 获取url的参数值,此处用?参数传参
     n = int(self.get_argument("n"))
+    # 使用阶乘服务
     self.write(str(self.service.calc(n)))
 
 def make_app():
